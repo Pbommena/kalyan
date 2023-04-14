6800e4f0f7694dac7ae498ba74e9474189cb7c56
pavan <pavan@PAVANKALYAN.mshome.net>
2023-04-14 11:00:20 -0500

    sub a sample

---
+++ src/main/java/pavan/Add.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package pavan;
+
+/**
+ * @author pavan
+ *
+ */
+public class Add {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a = 22;
+		int b = 35;
+		System.out.println(a-b);
+		// TODO Auto-generated method stub
+
+	}
+
+}
