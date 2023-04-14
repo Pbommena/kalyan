a017dabfaceb778d7261e184b51ec30c030027f1
pavan <pavan@PAVANKALYAN.mshome.net>
2023-04-14 10:24:07 -0500

    Sample files to push

---
+++ src/main/java/pavan/Sample.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package pavan;
+
+/**
+ * @author pavan
+ *
+ */
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 2;
+		int b=3;
+		
+		System.out.println(a+b);
+				
+
+	}
+
+}
